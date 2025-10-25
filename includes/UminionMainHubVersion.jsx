8bf04926519021782d45ede9db968f7d4ec9396f
Uminion <lahlalisalem@icloud.com>
2025-10-24 20:28:59 -0400

    Part 3 of X for:>>> GitHub Issue #34 is being upgraded with a NavBar MainHub
    added more content into the react page for github collab 003; specifically for instruction purposes.

--- includes/UminionMainHubVersion.jsx
+++ includes/UminionMainHubVersion.jsx
@@ -1,1 +1,11 @@
 import React from "react";
+import { createRoot } from "react-dom/client";
+
+
+
+
+
+
+
+const rootElement = document.getElementById("emojiLineForGitHubQuest34onFrontPage");
+const root = createRoot(rootElement);
