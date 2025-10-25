a36e6bcb964b1244d7616c3ac9efcfc9bb358aad
Uminion <lahlalisalem@icloud.com>
2025-10-24 20:45:23 -0400

    Part 4 of X For:> GitHub Issue #34 is being Upgraded with a NavBar for its MainHub
    adding more info into this GitHub Collab003 project for the simple purposes of educating what's going on. feel free ignoring this commit.

--- includes/UminionMainHubVersion.jsx
+++ includes/UminionMainHubVersion.jsx
@@ -4,8 +4,16 @@ import { createRoot } from "react-dom/client";
 
 
 
+const MyTestingComponent001 = () => {
+  return <p>loading...</p>
+};
+
+
 
 
 
 const rootElement = document.getElementById("emojiLineForGitHubQuest34onFrontPage");
 const root = createRoot(rootElement);
+
+root.render(<MyTestingComponent001 />);
+
