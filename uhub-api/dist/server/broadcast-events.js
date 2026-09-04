c95d8b5bd2681bdfffabbadf82ab53ce41b91d89
Uminion <lahlalisalem@icloud.com>
2026-09-04 03:18:51 -0400

    Revert "UpgradeFromV2intoV3 -92 Previous code crashed the launched uHub. this…"

--- uhub-api/dist/server/broadcast-events.js
+++
@@ -1,2 +0,0 @@
-import { EventEmitter } from 'events';
-export const broadcastEvents = new EventEmitter();
--- uhub-api/server/broadcast-events.ts
+++
@@ -1,3 +0,0 @@
-import { EventEmitter } from 'events';
-
-export const broadcastEvents = new EventEmitter();
